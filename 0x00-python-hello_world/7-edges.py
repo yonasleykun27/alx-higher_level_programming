004a8478584d7749f8ace33b3f1959f12c895716
yonas <yonasleykun27@gmail.com>
2022-09-19 18:16:26 +0000

    task 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -6,8 +6,3 @@ middle_word = word[1:-1]
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Secu
